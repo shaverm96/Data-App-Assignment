2dc6e7f7e455c093f47577409dfab2448e14f7e3
Matthew Shaver <119382144+shaverm96@users.noreply.github.com>
2025-08-05 20:18:34 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,7 +35,7 @@ categories = df["Category"].unique().tolist()
 selected_category = st.selectbox("Category", options=categories, index=0)
 
 st.write("### (2) add a multi-select for Sub_Category in the selected Category (1) (https://docs.streamlit.io/library/api-reference/widgets/st.multiselect)")
-df_sub_categories = df.loc[df["Category"].isin(selected_category), "Sub_Category"]
+df_sub_categories = df["Category"].isin(selected_category)
 sub_categories = df_sub_categories["Sub_Category"].unique().tolist()
 selected_sub_category = st.multiselect("Sub_Category", options=sub_categories, default=None)
 
